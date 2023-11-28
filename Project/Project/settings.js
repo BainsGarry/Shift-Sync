e7312a3e45aef370a2d6b274fb18c5cad4a25dc7
Gurkeerat Bains <92592979+BainsGarry@users.noreply.github.com>
2023-11-28 13:31:00 -0500

    Delete Project/Project directory

--- Project/Project/settings.js
+++
@@ -1,7 +0,0 @@
-// settings.js
-document.addEventListener('DOMContentLoaded', function() {
-    const themeSelect = document.querySelector('select[name="theme"]');
-    themeSelect.addEventListener('change', function() {
-        this.form.submit();
-    });
-});
